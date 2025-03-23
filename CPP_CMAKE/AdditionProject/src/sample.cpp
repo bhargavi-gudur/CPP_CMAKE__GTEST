3882daed46ca4d35e4420191326468534fac8623
Gandla Bhargavi <barau.avril@gmail.com>
2025-03-23 20:32:32 +0530

    add initial CMake configuration and source files for AdditionProject

---
+++ CPP_CMAKE/AdditionProject/src/sample.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file sample.cpp
+ * @bhargavi-gudur
+ * @brief  This is a sample source file
+ * @version 0.1
+ * @date 2025-03-11
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include "../include/sample.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
